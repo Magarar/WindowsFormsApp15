bf58bbbb53f0dacb41f0ef06d59e834ba1a9b473
Debu <156629227+Magarar@users.noreply.github.com>
2025-02-06 10:47:58 +0800

    Revert "132"
    This reverts commit 4c5f120e74381f6ce20fade1c1a610de26c74802.

--- Form1.cs
+++ Form1.cs
@@ -66,20 +66,5 @@ namespace WindowsFormsApp1
         {
 
         }
-
-        private void panel2_Paint(object sender, PaintEventArgs e)
-        {
-            throw new System.NotImplementedException();
-        }
-
-        private void label13_Click(object sender, EventArgs e)
-        {
-            throw new System.NotImplementedException();
-        }
-
-        private void label10_Click(object sender, EventArgs e)
-        {
-            throw new System.NotImplementedException();
-        }
     }
 }
